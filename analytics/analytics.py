86a188310775295a480b90953f080923d62ee373
VK <vk@Vanketeshs-MacBook-Air.local>
2023-08-27 16:17:06 +0530

    updated-docker-compose-and-data-generator

---
+++ analytics/analytics.py
@@ -0,0 +1,18 @@
+from os import environ
+from time import sleep
+from sqlalchemy import create_engine
+from sqlalchemy.exc import OperationalError
+
+print('Waiting for the data generator...')
+sleep(20)
+print('ETL Starting...')
+
+while True:
+    try:
+        psql_engine = create_engine(environ["POSTGRESQL_CS"], pool_pre_ping=True, pool_size=10)
+        break
+    except OperationalError:
+        sleep(0.1)
+print('Connection to PostgresSQL successful.')
+
+# Write the solution here
